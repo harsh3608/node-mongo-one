7b5f08f872b0d47d0071f9cde7b5005dc9535fee
Harsh Patel <harsh3608@gmail.com>
2023-08-18 17:58:03 +0530

    ...

--- app.js
+++ app.js
@@ -1,55 +1,58 @@
 const express = require("express");
-const mongodb = require('mongodb');   //connect to compass
-const mongoose = require('mongoose'); //connect to atlas
-var MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(express.json());
-
-// Configure body-parser middleware to parse request bodies
+app.use(cors()); // Allow all origins
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-//connecting to mongoose/ mongodb compass
-MongoClient.connect("mongodb://localhost:27017/BookStore", function (err, db) {
 
-    // if (err) throw err;
-
-    // //Write databse Insert/Update/Query code here..
-    // console.log('connected to database');
+//connecting to atlas
+async function run() {
 
 
-    db.collection('Books', function (err, collection) {
+    // const uri = "mongodb+srv://harshtemputil89:wiFUFPra1f4ZSVkx@cluster0.mftzekk.mongodb.net?retryWrites=true&w=majority";
 
-        collection.find().toArray(function (err, items) {
-            if (err) throw err;
-            console.log(items);
-        });
-
-    });
-
-});
-//mongoose.connect('mongodb://localhost:27017/');
+    const uri = "mongodb://mongodb://localhost:27017/";
 
 
+
+    const client = new MongoClient(uri);
+
+    await client.connect().then(console.log("connected")).catch(error => console.log(error));
+
+    const dbName = "BookStore";
+    const collectionName = "Books";
+
+    const database = client.db(dbName);
+    const collection = database.collection(collectionName);
+
+    collection.find({}).toArray((err, books) => {
+        if (err) {
+            console.error('Error fetching books:', err);
+        } else {
+            console.log('Books:', books);
+        }
+    })
+
+    await client.close();
+}
 
 const PORT = process.env.PORT || 3000;
-
-app.use(cors({
-    origin: '*'
-}));
-
-app.listen(PORT, () => {
-    console.log("Server Listening on PORT:", PORT);
-});
 
 app.get("/status", (request, response) => {
     const status = {
         Status: "Running",
     };
 
+    run();
+
     response.send(status);
+});
+
+app.listen(PORT, () => {
+    console.log("Server Listening on PORT:", PORT);
 });
